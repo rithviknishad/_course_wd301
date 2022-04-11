7c7ef4440a5bd35f39cb6f011de830062b3c8b27
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-11 19:56:04 +0530

    adds L4 milestone

--- my-app/src/AppContainer.tsx
+++ my-app/src/AppContainer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 export default function AppContainer(props: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen bg-gray-100 items-center">
       <div className="w-1/2 p-8 mx-auto bg-white shadow-lg rounded-xl">
+        <Header />
         {props.children}
       </div>
     </div>
