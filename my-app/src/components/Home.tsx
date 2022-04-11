a6590c6735b68595c13f86d1a53b8208ef335b3b
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-11 19:56:26 +0530

    adds L4 milestone

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,19 +1,20 @@
+import { Link } from "raviger";
 import React from "react";
 import logo from "../logo.svg";
 
-export function Home(props: { openFormCB: () => void }) {
+export default function Home() {
   return (
     <div className="flex flex-col justify-center">
       <img className="h-48" src={logo} alt="" />
       <div className="flex-1 flex justify-center items-center h-48">
         <p>Welcome to home page...</p>
       </div>
-      <button
+      <Link
         className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2"
-        onClick={props.openFormCB}
+        href="/forms"
       >
         Show Form
-      </button>
+      </Link>
     </div>
   );
 }
