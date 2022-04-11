b791ca68aa6f7d7e46ebb5510162ddddda56993b
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-11 19:56:08 +0530

    adds L4 milestone

--- my-app/src/Header.tsx
+++ my-app/src/Header.tsx
@@ -1,7 +1,8 @@
+import { ActiveLink } from "raviger";
 import React from "react";
 import logo from "./logo.svg";
 
-export default function Header(props: { title: string }) {
+export default function Header() {
   return (
     <div className="p-4 mx-auto flex flex-row">
       <img
@@ -10,7 +11,22 @@ export default function Header(props: { title: string }) {
         className="animate-spin w-20"
         style={{ animation: "spin 2s linear infinite" }}
       />
-      <h1 className="text-center text-xl">{props.title}</h1>
+      <div className="flex gap-2 items-center">
+        {[
+          { page: "Home", url: "/" },
+          { page: "About", url: "/about" },
+          { page: "Forms", url: "/forms" },
+        ].map((link) => (
+          <ActiveLink
+            key={link.url}
+            href={link.url}
+            className="text-gray-800 p-2 m-2 uppercase"
+            exactActiveClass="text-blue-800"
+          >
+            {link.page}
+          </ActiveLink>
+        ))}
+      </div>
     </div>
   );
 }
