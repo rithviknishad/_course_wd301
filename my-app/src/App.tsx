c6f336f962268695e84e7b7659ea501482463883
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-11 19:56:00 +0530

    adds L4 milestone

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
-import Header from "./Header";
-
-import AppContainer from "./AppContainer";
-import { Home } from "./components/Home";
-import { FormsList } from "./components/FormsList";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  const [state, setState] = useState("HOME");
-
-  const openForm = () => {
-    setState("FORM");
-  };
-
-  const closeForm = () => {
-    setState("HOME");
-  };
-
-  return (
-    <AppContainer>
-      <Header
-        title={"Welcome to Lesson 5 of $react-typescript with tailwindcss"}
-      />
-      {state === "HOME" ? (
-        <Home openFormCB={openForm} />
-      ) : (
-        <FormsList closeFormCB={closeForm} />
-      )}
-    </AppContainer>
-  );
+  return <AppRouter />;
 }
 
 export default App;
