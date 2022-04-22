c9ea7f5a80296d3c291444ed9f5330da69cd5906
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-22 20:24:31 +0530

    configure sentry

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import reportWebVitals from "./reportWebVitals";
+
+Sentry.init({
+  dsn: "https://1f5c4d46efac42bea7bd92f51cdecfbf@o1206520.ingest.sentry.io/6356419",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
