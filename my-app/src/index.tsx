70dc8e41d51080d92832617ce82b2618f0b1ef26
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-25 20:48:12 +0530

    setup service worker and registration

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 if (process.env.REACT_APP_SENTRY_DSN) {
@@ -24,6 +25,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
